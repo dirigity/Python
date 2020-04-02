6b53e3bd101970c34a03700f2cddfd3f981e014a
dirigity <dirigityjgf@gmail.com>
2020-04-02 23:08:06 +0200

    cubes!!

--- RayMarcher/Main.py
+++ RayMarcher/Main.py
@@ -6,22 +6,24 @@ from PIL import Image
 from multiprocessing import Pool
 
 import sys
-sys.stdout = open("Out.txt", "w")
 
 
 
 
-# ObjetName,SizeMax,posX,posY,posZ
-# Sfere,Rad,posX,PosY,Posz
-# Point,PosX,PosY,PosZ,I,r
+# ObjetName,Bounds,posX,posY,posZ,R,G,B,Rough,additional
+# Sfere,r,posX,PosY,Posz,R,G,B,rough
+# Cube,Bounds,posX,PosY,Posz,R,G,B,rough
+
+
+# Point,PosX,PosY,PosZ,rad,R,G,B,Bri
 # Sun,AngleX,AngleY,I
-SKY = [70,70,70]#[0,160,250]
+SKY = [70,70,70]##[0,160,250]
 CERO = 0.000001
 INF = 1000000000
-maxSteps = 100
+maxSteps = 1000
 maxDistance = 1000
-objects=[["Sfere",1,0,0,0],["Sfere",1,0,0,1],["Sfere",1,0,0,2]]
-lights=[["Point",2,1,0,1,0.3]]
+objects=[["Sfere",1,0,0,0,255,100,255,1],["Cube",(1,1,10),0,-1,0,100,255,255,1],["Sfere",1,0,0,2,255,255,100,1]]
+lights=[["Point",-2,1,0,0.3,255,255,255,3]]
 
 
 def VectorFromAngle(aX,aY):
@@ -32,42 +34,44 @@ def VectorFromAngle(aX,aY):
     z=m1.sin(aX)
     return (x,y,z)
 
-def Ray(cx,cy,cz,V,bounces):
-    Vx = V[0]
-    Vy = V[1]
-    Vz = V[2]
+def Ray(cx,cy,cz,dir,bounces):
     x = cx
     y = cy
     z = cz
-    dir = [Vx,Vy,Vz]
     OBJ = []
     #Chocar con objeto
     hit = False
     StepCounter = 0
-    maxDistance = MAXdistance(z,y,x)
-    while not hit and maxSteps > StepCounter and maxDistance > PointDistance(x,y,z,cx,cy,cz):
-        d = MINdistance(x,y,z)
+    maxDistance = 100 #MAXdistance(z,y,x)
 
-        x = x + dir[0]*d[0] 
-        y = y + dir[1]*d[0]
-        z = z + dir[2]*d[0]
-        if(d[0]<CERO):
+    while not hit and maxSteps > StepCounter and maxDistance > PointDistance(x,y,z,cx,cy,cz):
+        dInfo = MINdistance(x,y,z)
+        d = dInfo[0]
+        x = x + dir[0]*d
+        y = y + dir[1]*d
+        z = z + dir[2]*d
+        if(d<CERO):
             hit = True
-            OBJ = d[1]
+            OBJ = objects[dInfo[1]]
+            x = x - dir[0]*d
+            y = y - dir[1]*d
+            z = z - dir[2]*d
         StepCounter = StepCounter + 1 
 
-               
-
-    #ir a luces
-
-    colorB = 0
-
     if not hit:
-        return SKY
-    else:
+        return SKY           
+
+    SurfaceR = OBJ[5]/255
+    SurfaceG = OBJ[6]/255
+    SurfaceB = OBJ[7]/255
+
+    colorR = SKY[0]*SurfaceR
+    colorG = SKY[1]*SurfaceG
+    colorB = SKY[2]*SurfaceB
+    if bounces is 0:
+        #ir a luces
         for lam in lights:
             V = [0,0,1]
-            intensity = 0
 
             if(lam[0]=="Point"):
                 vx = -(x-lam[1])
@@ -75,42 +79,68 @@ def Ray(cx,cy,cz,V,bounces):
                 vz = -(z-lam[3])
                 l = PointDistance(0,0,0,vx,vy,vz)
                 V = [vx/l,vy/l,vz/l]
-                intensity = lam[4]
+                intensityR = lam[5]
+                intensityG = lam[6]
+                intensityB = lam[7]
+                brightness = lam[8]
             
                 hit = False
                 ilum = False
-                lx = x + vx * 0.1
-                ly = y + vy * 0.1
-                lz = z + vz * 0.1
+                lx = x + vx * 0.01
+                ly = y + vy * 0.01
+                lz = z + vz * 0.01
 
                 while not hit and not ilum:
 
 
                     d = MINdistanceObjLamp(lx,ly,lz,lam)
 
-                    lx = lx + V[0]*(d+CERO)     
-                    ly = ly + V[1]*(d+CERO)
-                    lz = lz + V[2]*(d+CERO)
+                    lx = lx + V[0]*d
+                    ly = ly + V[1]*d
+                    lz = lz + V[2]*d
 
                     if d<CERO :
                         hit = True
-                    elif PointDistance(lx,ly,lz,lam[1],lam[2],lam[3])<lam[5] :
+                    elif PointDistance(lx,ly,lz,lam[1],lam[2],lam[3])<lam[4] :
                         ilum = True
-                        colorB = colorB + abs(intensity * m1.cos(angle(V,NormalEsf(x,y,z,OBJ))) * 1/PointDistance(x,y,z,lam[1],lam[2],lam[3])) 
-            
+                        colorR = colorR + SurfaceR*abs(intensityR*brightness * m1.cos(angle(V,Normal(x,y,z,OBJ))) * 1/PointDistance(x,y,z,lam[1],lam[2],lam[3])) 
+                        
+                        colorG = colorG + SurfaceG*abs(intensityG*brightness * m1.cos(angle(V,Normal(x,y,z,OBJ))) * 1/PointDistance(x,y,z,lam[1],lam[2],lam[3])) 
 
+                        colorB = colorB + SurfaceB*abs(intensityB*brightness * m1.cos(angle(V,Normal(x,y,z,OBJ))) * 1/PointDistance(x,y,z,lam[1],lam[2],lam[3])) 
                     
-    colorB = colorB * ((1/(StepCounter+1)/50)+1)
-    r = min(250*colorB+SKY[0]*((1/(StepCounter+1)/1000)+1),250)
-    g = min(250*colorB+SKY[1]*((1/(StepCounter+1)/1000)+1),250)
-    b = min(250*colorB+SKY[2]*((1/(StepCounter+1)/1000)+1),250)
-    return [r,g,b]
+                    
+        #implement ambient Oclusion
+        r = min(colorR*((1/(StepCounter+1)/1000)+1),250)
+        g = min(colorG*((1/(StepCounter+1)/1000)+1),250)
+        b = min(colorB*((1/(StepCounter+1)/1000)+1),250)
+        return (r,g,b)
+    else:
+        bounces = bounces - 1
+        bounceVecotors = bounceVectors(dir,normalizeTuple(Normal(x,y,z,OBJ)),OBJ[8])
+        r = SKY[0]*SurfaceR
+        g = SKY[1]*SurfaceG
+        b = SKY[2]*SurfaceB
+        for VBounce in bounceVecotors:
+            c = Ray(x,y,z,VBounce,bounces)
+            r = min(r + c[0] ,250) #* ((SurfaceR-1)/1)+1)
+            g = min(g + c[1] ,250) #* ((SurfaceG-1)/1)+1)
+            b = min(b + c[2] ,250) #* ((SurfaceB-1)/1)+1)
+        return (r,g,b)
+
+def bounceVectors(Vdir,Vnor,R):
+    l = len(Vdir)
+    #Vdir -( 2 * (Vdir * Vnor )*Vnor)
+    Res2 = tuple(dotproduct(Vdir,Vnor)*Vnor[j] for j in range(l))
+    Res1 = tuple(2*Res2[j] for j in range(l))
+    center = tuple(Vdir[i] - Res1[i] for i in range(l))
+    return [center]
 
 def dotproduct(v1, v2):
     return sum((a*b) for a, b in zip(v1, v2))
 
 def length(v):
-    return m1.sqrt(dotproduct(v, v))
+    return (dotproduct(v, v))**.5
 
 def angle(v1, v2):
     a = dotproduct(v1, v2) / (length(v1) * length(v2))
@@ -121,19 +151,50 @@ def angle(v1, v2):
     return m1.acos(a)
 
 def PointDistance(x1,y1,z1,x2,y2,z2):
-    return ((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**.5
+    A = x1-x2
+    B = y1-y2
+    C = z1-z2
+    return (A*A + B*B + C*C)**.5
 
-
-
-def NormalEsf(x,y,z,OBJ):
+def Normal(x,y,z,OBJ):
     # Sfere,Rad,posX,PosY,Posz
-    return[x-OBJ[2],y-OBJ[3],z-OBJ[4]]
+    if(OBJ[0]=="Sfere"):
+        rX = (x-OBJ[2])
+        rY = (y-OBJ[3])
+        rZ = (z-OBJ[4])
+        return (rX,rY,rZ)
+    elif(OBJ[0]=="Cube"):
+        s = OBJ[1]
+        rX = (x-OBJ[2])/(s[0]/2)
+        rY = (y-OBJ[3])/(s[1]/2)
+        rZ = (z-OBJ[4])/(s[2]/2)
+
+        vx = 0
+        vy = 0
+        vz = 0
+
+        if(rX>1):
+            vx = 1
+        elif(rX<-1):
+            vx = -1
+        
+        if(rY>1):
+            vy = 1
+        elif(rY<-1):
+            vy = -1
+        
+        if(rZ>1):
+            vz = 1
+        elif(rZ<-1):
+            vz = -1
+        
+        return (vx,vy,vz)
+
     
 
 
 def MINdistanceObjLamp(x,y,z,l):
     return min(PointDistance(x,y,z,l[1],l[2],l[3]),MINdistance(x,y,z)[0])
-    
 
 def MINdistance(x,y,z):
     rets = distanceList(x,y,z)
@@ -146,14 +207,14 @@ def MINdistance(x,y,z):
             ret = r
         Index = Index+1
     
-    return [ret,objects[I]]
+    return [ret,I]
 
 def MAXdistance(x,y,z):
     rets = distanceList(x,y,z)
     ret = 0
 
     for rindex in range(len(rets)):
-        r = rets[rindex]+objects[rindex][1]*2
+        r = rets[rindex]+length(objects[rindex][1])*2
         if(r>ret): 
             ret = r
     
@@ -161,12 +222,23 @@ def MAXdistance(x,y,z):
 
 def distanceTo(obj,x,y,z):
     if(obj[0]=="Sfere"):
-        return (PointDistance(x,y,z,obj[2],obj[3],obj[4])-obj[1])
+
+        return PointDistance(x,y,z,obj[2],obj[3],obj[4])-(obj[1]/2)
+    if(obj[0]=="Cube"):
+        #max{0,|x|−1}^2+max{0,|y|−1}^2+max{0,|z|−1}^2
+        s = obj[1]
+        rX = (x-obj[2])/(s[0]/2)
+        rY = (y-obj[3])/(s[1]/2)
+        rZ = (z-obj[4])/(s[2]/2)
+        pX = max(0,abs(rX)-1)
+        pY = max(0,abs(rY)-1)
+        pZ = max(0,abs(rZ)-1)
+        return PointDistance(0,0,0,pX,pY,pZ)
+
     return INF
 
 
 def distanceList(x,y,z):
-     
     return [distanceTo(obj,x,y,z) for obj in objects]
 
 def render(x,y,z,multiProz,w,h):
@@ -206,6 +278,7 @@ def render(x,y,z,multiProz,w,h):
 # 0 1 2 3 4 5
 #(b,x,y,z,w,h)
 def threadRay(d):
+    
     w=d[4]
     h=d[5]
     antires = 1/100
@@ -222,12 +295,19 @@ def threadRay(d):
     #    Vx = -1
     #    Vy = ((columnN/h)*sensorH)-(sensorH/2)
     #    Vz = ((elementN/w)*sensorW)-(sensorW/2)
-    #    ret.append(Ray(x,y,z,normalice(Vx,Vy,Vz),0))
+    #    ret.append(Ray(x,y,z,normalize(Vx,Vy,Vz),0))
     #    elementN = elementN+1
 
-    return [ Ray(x,y,z,normalice(-1,((columnN/h)*sensorH)-(sensorH/2),((elementN/w)*sensorW)-(sensorW/2)),0) for elementN in range(w)]
+    return [ Ray(x,y,z,normalize(-1,((columnN/-h)*sensorH)+(sensorH/2),((elementN/w)*sensorW)-(sensorW/2)),0) for elementN in range(w)]
 
-def normalice(x,y,z):
+def normalize(x,y,z):
+    l = PointDistance(0,0,0,x,y,z)
+    return (x/l,y/l,z/l)
+
+def normalizeTuple(V):
+    x = V[0]
+    y = V[1]
+    z = V[2]
     l = PointDistance(0,0,0,x,y,z)
     return (x/l,y/l,z/l)
 
@@ -235,6 +315,7 @@ def main():
     aspectRatio = (16,9)
     m = 20
     img = render(2,0,0,False,aspectRatio[0]*m,aspectRatio[1]*m)
+    sys.stdout = open("Out.txt", "w")
     img.save('render.png')
 
     img.show()
