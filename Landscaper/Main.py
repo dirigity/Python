1f6240d6b689a64f2dbb3c84fe7d239fff09fe3c
dirigity <dirigityjgf@gmail.com>
2020-04-06 18:10:15 +0200

    asdf

--- Landscaper/Main.py
+++ Landscaper/Main.py
@@ -4,68 +4,16 @@ import random
 import math as m
 import numpy as np
 import random
+#from Landscaper 
+import PerlingNoiseMagic as PN
 
+random.seed(0)
 
-size, freq, octs = 500, 1/200, 3 #smaler freq means biguer mountains 
-
-
+size = 500 # Size of the map (in pixels)
+scale = 3 # afects the size of the noise(biguer scale, smaler wrincles), only in tileable mode
+tileable = True #Makes the map tileable, but it slows the proces majorly (also the slope detection doesnt work on not tilible borders, so you will need to remove a litle bit from the border of the image manualy or proceduraly on your 3d sofware of choice)
 DisolveConstant = 1/10 # Should be betwen 0 and 1 highter makes deeper trenches as drops walk
 EvaporationRate = 0.2 # bigger than 0
-
-def noiseArrNoTileable():
-
-
-    shape = (500,500) # texture size (Height,Width)
-    scale = 400.0
-    octaves = 4
-    persistence = 0.5
-    lacunarity = 2.0
-
-    n = 0
-    world = [[0 for y in range(size)]for x in range(size)]
-    for i in range(size):
-        for j in range(size):
-            n = noise.pnoise2(i/scale, 
-                              j/scale, 
-                              octaves=octaves, 
-                              persistence=persistence, 
-                              lacunarity=lacunarity, 
-                              repeatx=1024, 
-                              repeaty=1024, 
-                              base=0)
-            world[i][j] = n
-            
-    return world
-
-perm = list(range(size*2))
-random.shuffle(perm)
-perm += perm
-dirs = [(m.cos(a * 2.0 * m.pi / size*2),
-        m.sin(a * 2.0 * m.pi / size*2))
-        for a in range(size*2)]
-
-def noise(x, y, per):
-    def surflet(gridX, gridY):
-        distX, distY = abs(x-gridX), abs(y-gridY)
-        polyX = 1 - 6*distX**5 + 15*distX**4 - 10*distX**3
-        polyY = 1 - 6*distY**5 + 15*distY**4 - 10*distY**3
-        hashed = perm[perm[int(gridX)%per] + int(gridY)%per]
-        grad = (x-gridX)*dirs[hashed][0] + (y-gridY)*dirs[hashed][1]
-        return polyX * polyY * grad
-    intX, intY = int(x), int(y)
-    return (surflet(intX+0, intY+0) + surflet(intX+1, intY+0) +
-            surflet(intX+0, intY+1) + surflet(intX+1, intY+1))
-
-def fBm(x, y, per, octs):
-    val = 0
-    for o in range(octs):
-        val += 0.5**o * noise(x*2**o, y*2**o, per*2**o)
-    return val
-
-
-
-
-
 
 
 def findSlope(x,y,map,OriginalLevel):
@@ -80,22 +28,30 @@ def findSlope(x,y,map,OriginalLevel):
     [(x+1)  %size,(y+1) %size]
     ]
     ret = [x%size,y%size]
+    retValue = map[x%size][y%size]
+    retOriginalValue = OriginalLevel[x%size][y%size]
     for pos in rets:
-        if 10*OriginalLevel[pos[0]][pos[1]]+map[pos[0]][pos[1]] < 10*OriginalLevel[ret[0]][ret[1]]+map[ret[0]][ret[1]]:
+        posValue = map[ret[0]][ret[1]]
+        posOriginalValue = OriginalLevel[ret[0]][ret[1]]
+        if 10*max(0,posOriginalValue-posValue)+posValue < 10*max(0,retOriginalValue-retValue)+retValue:
             ret = pos
+            retValue = posValue
+            retOriginalValue = posOriginalValue
 
     return ret
 
 def main():
 
     print("Generating Ground Noise")
-    Height = [[fBm(x*freq, y*freq, int(size*freq), octs) for x in range(size)] for y in range(size)]
-
-
+    pnFact = PN.PerlinNoiseFactory(4,octaves = 4) #greater octaves makes more detailed noise at cost of speed
+    if tileable :
+        Height = [[pnFact(m.cos((j/size)*2*m.pi)/scale,m.sin((j/size)*2*m.pi)/scale,m.cos((i/size)*2*m.pi)/scale,m.sin((i/size)*2*m.pi)/scale) for i in range(size)] for j in range(size)]
+    else:
+        Height = [[pnFact(0,0,i,j) for i in range(size)] for j in range(size)]
     Height = normaliceArr(Height,100)
     OriginalLevel = copy.deepcopy(Height) # Used for takin into acount the top soil level in terrain solubility
 
-    itterations = 1000 # number of rain droplets to simulate
+    itterations = 100 # number of rain droplets to simulate
     print("Starting Erosion")
 
     I = 0
@@ -103,12 +59,14 @@ def main():
         #create droplet at random place
         DropX = random.randint(0,size)%size
         DropY = random.randint(0,size)%size
-        waterContent = 300 # one Water content can disolve one Soil content && one water content will be removed from the droplet one each 1/EvaporationRate steps
+        waterContent = 500 # one Water content can disolve one Soil content && one water content will be removed from the droplet one each 1/EvaporationRate steps
         SoilContent = 0
         while (waterContent>0):
             #print (SoilContent,waterContent)
             #put in the ground if saturated
             if SoilContent > waterContent:
+                if((SoilContent-waterContent)>1):
+                    print("sedimenting:"+str((SoilContent-waterContent)))
                 Height[DropX][DropY] = Height[DropX][DropY]+(SoilContent-waterContent)
                 SoilContent = waterContent
 
@@ -140,11 +98,13 @@ def main():
             print(I/itterations)
             d = 10
             if(I%(itterations/d)==0):
-                print("-----")
+                print("---TakingSnapShot---")
                 imageFromHeight(Height,(I/itterations)*d)
 
         I = I + 1
     DiferenceMap = [[Height[x][y]-OriginalLevel[x][y] for y in range(size)]for x in range(size)]
+
+    print("Procesing Output")
 
     imageFromHeight(Height,"Final")
     imageFromHeight(DiferenceMap,"Difference")
@@ -156,7 +116,9 @@ def normaliceArr (arr,NewMax):
             v.append(e)
     Min = min(v)
     Max = max(v)
-    return np.array([ [ NewMax*((e-Min)/(Max-Min)) for e in row ] for row in arr])
+    if(Min==Max):
+        return np.array(arr)
+    return np.array([ [ NewMax*((o-Min)/(Max-Min)) for o in row ] for row in arr])
 
 def imageFromHeight(Height,tag):
     #Normalice and convert to image (as we only hace 256 posible hight states we normalice to get the maximum detail out of them)
